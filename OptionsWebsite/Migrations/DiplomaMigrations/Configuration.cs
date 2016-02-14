8b99de9214398604311c3484efb715f71cda4072
Jon Deluz <jondeluz@hotmail.com>
2016-02-13 17:52:32 -0800

    Updated config to seed the database

--- OptionsWebsite/Migrations/DiplomaMigrations/Configuration.cs
+++ OptionsWebsite/Migrations/DiplomaMigrations/Configuration.cs
@@ -1,6 +1,8 @@
 namespace OptionsWebsite.Migrations.DiplomaMigrations
 {
+    using DiplomaDataModel;
     using System;
+    using System.Collections.Generic;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
@@ -15,18 +17,23 @@ namespace OptionsWebsite.Migrations.DiplomaMigrations
 
         protected override void Seed(DiplomaDataModel.Diploma.DiplomasContext context)
         {
-            //  This method will be called after migrating to the latest version.
+            List<YearTerm> yearterms = new List<YearTerm>() {
+              new YearTerm {Year=2015, Term = 20, isDefault=false},
+              new YearTerm {Year=2015, Term = 30, isDefault=false},
+              new YearTerm {Year=2016, Term = 10, isDefault=false},
+              new YearTerm {Year=2016, Term = 30, isDefault=true},
+            };
+            context.YearTerms.AddOrUpdate(s => s.YearTermID, yearterms.ToArray());
 
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data. E.g.
-            //
-            //    context.People.AddOrUpdate(
-            //      p => p.FullName,
-            //      new Person { FullName = "Andrew Peters" },
-            //      new Person { FullName = "Brice Lambson" },
-            //      new Person { FullName = "Rowan Miller" }
-            //    );
-            //
+            List<Option> options = new List<Option>() {
+              new Option {Title="Data Communications", isActive=true},
+              new Option {Title="Client Server", isActive=true},
+              new Option {Title="Digital Processing", isActive=true},
+              new Option {Title="Informations Systems", isActive=true},
+              new Option {Title="Database", isActive=false},
+              new Option {Title="Web and Mobile", isActive=true},
+            };
+            context.Options.AddOrUpdate(o => o.OptionID, options.ToArray());
         }
     }
 }
