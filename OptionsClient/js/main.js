c20d73dfb6c4e097bbb0208267296eac3a9beec9
Anthony Smith <anthony@smith.lt>
2016-03-25 14:37:41 -0700

    Add all required views for angular app (No functionality yet)

--- OptionsClient/js/main.js
+++ OptionsClient/js/main.js
@@ -1,18 +1,61 @@
 ﻿var app = angular.module('choicesApp', ['ngRoute']);
 
-// setup the routing
+// Setup the routing
 app.config(function ($routeProvider) {
 
     $routeProvider
-      // display all of the students
       .when('/', {
           templateUrl: 'pages/home.html',
-          controller: 'mainController'
+          controller: 'mainController',
+          title: 'Home'
       })
+      .when('/login', {
+          templateUrl: 'pages/login.html',
+          controller: 'loginController',
+          title: 'Login'
+      })
+      .when('/register', {
+          templateUrl: 'pages/register.html',
+          controller: 'registerController',
+          title: 'Register'
+      })
+      .when('/submit', {
+          templateUrl: 'pages/submit.html',
+          controller: 'submitController',
+          title: 'Submit Choice'
+      });
 
 });
 
-// controller for the home page
+// Controls the rootscope
+app.run(function ($rootScope, $route) {
+    $rootScope.$on("$routeChangeSuccess", function (currentRoute, previousRoute) {
+        //Change page title, based on Route information
+        $rootScope.title = $route.current.title;
+    });
+});
+
+// Maintains the header (changes based on user authenticated or not)
+app.controller('headerController', function ($scope) {
+    $scope.hello = "world";
+});
+
+// Controller for the home page
 app.controller('mainController', function ($scope, $http) {
     $scope.message = 'Everyone come and look!';
+});
+
+// Controller for the login page
+app.controller('loginController', function($scope, $http) {
+    $scope.message = 'Login Page';
+});
+
+// Controller for the register page
+app.controller('registerController', function ($scope, $http) {
+    $scope.message = 'Register Page';
+});
+
+// Controller for the submission page
+app.controller('submitController', function ($scope, $http) {
+    $scope.message = 'Submit Choice';
 });
