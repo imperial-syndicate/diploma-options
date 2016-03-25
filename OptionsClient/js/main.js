e9be435229c44da33936a1711631b5f9856d1090
Anthony Smith <anthony@smith.lt>
2016-03-25 11:01:12 -0700

    Add basic angular foundation

---
+++ OptionsClient/js/main.js
@@ -0,0 +1,18 @@
+﻿var app = angular.module('choicesApp', ['ngRoute']);
+
+// setup the routing
+app.config(function ($routeProvider) {
+
+    $routeProvider
+      // display all of the students
+      .when('/', {
+          templateUrl: 'pages/home.html',
+          controller: 'mainController'
+      })
+
+});
+
+// controller for the home page
+app.controller('mainController', function ($scope, $http) {
+    $scope.message = 'Everyone come and look!';
+});
